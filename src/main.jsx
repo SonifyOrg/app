ddc5efbb337552ddb8d893105e1e4cc2fb564d75
tikrack <tikrackcode@gmail.com>
2025-08-17 13:59:16 +0330

    [Resolve] -> handle src and routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,4 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Routes from "@/components/Routes.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<Routes/>);
