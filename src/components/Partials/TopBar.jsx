1b4cf61a45b3aa5514ad5ba09f9170bfbc741955
tikrack <tikrackcode@gmail.com>
2025-08-17 15:32:25 +0330

    [Border] -> handle and add border colors

--- src/components/Partials/TopBar.jsx
+++ src/components/Partials/TopBar.jsx
@@ -4,6 +4,7 @@ import {useEffect} from "react";
 const appWindow = getCurrentWindow();
 
 const TopBar = () => {
+    const buttonClasses = "size-[15px] rounded-full cursor-pointer";
 
     const handleMinimize = () => {
         appWindow.minimize()
@@ -18,43 +19,13 @@ const TopBar = () => {
     }
 
     return (
-        <div className="titlebar">
-            <div data-tauri-drag-region></div>
-            <div className="controls">
-                <button id="titlebar-minimize" title="minimize" onClick={handleMinimize}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                    >
-                        <path fill="currentColor" d="M19 13H5v-2h14z"/>
-                    </svg>
-                </button>
-                <button id="titlebar-maximize" title="maximize" onClick={handleMaximize}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                    >
-                        <path fill="currentColor" d="M4 4h16v16H4zm2 4v10h12V8z"/>
-                    </svg>
-                </button>
-                <button id="titlebar-close" title="close" onClick={handleClose}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                    >
-                        <path
-                            fill="currentColor"
-                            d="M13.46 12L19 17.54V19h-1.46L12 13.46L6.46 19H5v-1.46L10.54 12L5 6.46V5h1.46L12 10.54L17.54 5H19v1.46z"
-                        />
-                    </svg>
-                </button>
+        <div className="titlebar px-3 h-10 bg-dark-2 flex justify-between">
+            <div className="controls flex items-center gap-2">
+                <button id="titlebar-close" title="close" onClick={handleClose} className={`${buttonClasses} bg-[#FC5753]`}></button>
+                <button id="titlebar-minimize" title="minimize" onClick={handleMinimize} className={`${buttonClasses} bg-[#FDBC40]`}></button>
+                <button id="titlebar-maximize" title="maximize" onClick={handleMaximize} className={`${buttonClasses} bg-[#36C84B]`}></button>
             </div>
+            <div data-tauri-drag-region className={"flex-1"}></div>
         </div>
     )
 }
