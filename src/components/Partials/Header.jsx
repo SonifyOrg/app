a62b7a4a0e15259e7035fc4f911c055d7847b267
tikrack <tikrackcode@gmail.com>
2025-08-18 03:04:45 +0330

    [Github] -> add github button

--- src/components/Partials/Header.jsx
+++ src/components/Partials/Header.jsx
@@ -2,20 +2,33 @@ import Spotify from "@/icons/Spotify.jsx";
 import {Link} from "react-router";
 import HomeIcon from "@/icons/HomeIcon.jsx";
 
-const Header = () => (<>
-    <header className={" w-full h-12 my-2 flex px-4"}>
-        <div className="border flex-1"></div>
-        <div className="flex-1 flex justify-center items-center">
-            <Link to="/" className={"size-12 bg-dark-2 rounded-full flex justify-center items-center transition-all duration-200 active:scale-100 hover:scale-105 active:opacity-75"}>
-                <HomeIcon />
-            </Link>
-        </div>
-        <div className="flex-1 flex items-center justify-end">
-            <Link to="/">
-                <Spotify/>
-            </Link>
-        </div>
-    </header>
-</>)
+const Header = () => {
+    const handleGithub = () => {
+
+    }
+
+    return <>
+        <header className={" w-full h-12 my-2 flex px-4"}>
+            <div className="flex-1 flex">
+                <button
+                    className={"bg-white active:scale-100 hover:scale-105 active:opacity-75 cursor-pointer transition-all duration-200 text-black min-w-[100px] rounded-2xl font-bold"}
+                    onClick={handleGithub}>Github
+                </button>
+            </div>
+            <div className="flex-1 flex justify-center items-center">
+                <Link to="/"
+                      className={"size-12 bg-dark-2 rounded-full flex justify-center items-center transition-all duration-200 active:scale-100 hover:scale-105 active:opacity-75"}>
+                    <HomeIcon/>
+                </Link>
+            </div>
+            <div className="flex-1 flex items-center justify-end">
+                <Link to="/"
+                      className={"active:scale-100 hover:scale-105 active:opacity-75 transition-all duration-200"}>
+                    <Spotify/>
+                </Link>
+            </div>
+        </header>
+    </>
+}
 
 export default Header
