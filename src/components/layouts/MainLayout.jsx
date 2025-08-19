5a8097c9b3b43569b03b5b7894f6887a8b12e09a
tikrack <tikrackcode@gmail.com>
2025-08-19 12:56:31 +0330

    [Name & Title] -> added

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -12,14 +12,20 @@ const MainLayout = ({children}) => {
     return (<>
         <div className={"flex justify-center"}>
             <div
-                className={`${!musicAvailable && "translate-x-[calc(450px/2)]"} transition-all duration-200 basis-[calc(100%-400px)]`}>
+                className={`${!musicAvailable && "translate-x-[calc(400px/2)]"} transition-all duration-200 basis-[calc(100%-400px)]`}>
                 <Header/>
-                <div className={"px-4 border py-5 mx-auto"}>
+                <div className={"px-4 py-5 mx-auto"}>
                     {children}
                 </div>
             </div>
             <div
-                className={`${!musicAvailable && "translate-x-56 opacity-0"} basis-[450px] overflow-hidden transition-all duration-200 bg-dark-2`}>
+                className={`${!musicAvailable && "translate-x-56 opacity-0"} basis-[400px] overflow-hidden transition-all duration-200 bg-dark-2 h-screen`}>
+                <img src={`http://141.11.37.179:5000/${music?.cover}`} alt=""
+                     className={"aspect-square w-full object-cover"}/>
+                <div className={"px-4 pt-4"}>
+                    <h3 className={"text-3xl font-bold hover:underline cursor-pointer"}>{music?.title}</h3>
+                    <h4 className={"text-gray-400 hover:underline cursor-pointer mt-2"}>{music?.artist}</h4>
+                </div>
             </div>
         </div>
     </>)
