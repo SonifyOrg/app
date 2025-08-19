ec876027f750cda2551b13722d50a3550653599d
tikrack <tikrackcode@gmail.com>
2025-08-19 15:46:52 +0330

    [Icon] -> add icon

---
+++ src/icons/PauseIcon.jsx
@@ -0,0 +1,12 @@
+const PauseIcon = () => (<>
+    <svg viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" className={"size-5"}>
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+        <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+        <g id="SVGRepo_iconCarrier">
+            <path d="M7 1H2V15H7V1Z" fill="#000000"></path>
+            <path d="M14 1H9V15H14V1Z" fill="#000000"></path>
+        </g>
+    </svg>
+</>)
+
+export default PauseIcon
