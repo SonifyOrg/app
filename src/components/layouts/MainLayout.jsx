9889720f28f932b45dccbd6a8c68bde2dcb2021f
tikrack <tikrackcode@gmail.com>
2025-08-19 13:06:44 +0330

    [Audio] -> add audio package

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -19,13 +19,16 @@ const MainLayout = ({children}) => {
                 </div>
             </div>
             <div
-                className={`${!musicAvailable && "translate-x-56 opacity-0"} basis-[400px] overflow-hidden transition-all duration-200 bg-dark-2 h-screen`}>
+                className={`${!musicAvailable && "translate-x-56 opacity-0"} relative basis-[400px] overflow-hidden transition-all duration-200 bg-dark-2 h-screen`}>
                 <img src={`http://141.11.37.179:5000/${music?.cover}`} alt=""
                      className={"aspect-square w-full object-cover"}/>
                 <div className={"px-4 pt-4"}>
                     <h3 className={"text-3xl font-bold hover:underline cursor-pointer"}>{music?.title}</h3>
                     <h4 className={"text-gray-400 hover:underline cursor-pointer mt-2"}>{music?.artist}</h4>
                 </div>
+                <footer className={"w-full border absolute bottom-0 right-0 min-h-32 px-4"}>
+//Autdio Playerac
+                </footer>
             </div>
         </div>
     </>)
